e56b99b15608df254b887793f51afbf3031f40da
Red3ffect <red3ffect117@gmail.com>
2021-05-23 20:38:24 -0700

    Adds Engineer object and methods. Also updates Employee object & methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,7 @@
 const Employee = require('../lib/Employee.js');
 
 test('create employee object', () => {
-    const employee = new Employee('Dave', 0, 'asdf@gmail.com', 'Employee');
+    const employee = new Employee('Dave', 0, 'asdf@gmail.com');
 
     expect(employee.name).toBe('Dave');
     expect(employee.id).toEqual(expect.any(Number));
@@ -10,25 +10,25 @@ test('create employee object', () => {
 });
 
 test('gets employee name', () => {
-    const employee = new Employee('Dave', 0, 'asdf@gmail.com', 'Employee');
+    const employee = new Employee('Dave', 0, 'asdf@gmail.com');
 
     expect(employee.getName()).toEqual('Dave');
 });
 
 test('gets employee ID number', () => {
-    const employee = new Employee('Dave', 0, 'asdf@gmail.com', 'Employee');
+    const employee = new Employee('Dave', 0, 'asdf@gmail.com');
 
     expect(employee.getId()).toEqual(expect.any(Number));
 });
 
 test('gets employee email', () => {
-    const employee = new Employee('Dave', 0, 'asdf@gmail.com', 'Employee');
+    const employee = new Employee('Dave', 0, 'asdf@gmail.com');
 
     expect(employee.getEmail()).toBe('asdf@gmail.com');
 });
 
 test("gets employee's role", () => {
-    const employee = new Employee('Dave', 0, 'asdf@gmail.com', 'Employee');
+    const employee = new Employee('Dave', 0, 'asdf@gmail.com');
 
     expect(employee.getRole()).toBe('Employee');
 });
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,34 +1,41 @@
 const Engineer = require('../lib/Engineer');
 
-test('create employee object', () => {
-    const engineer = new Engineer('Dave', 0, 'asdf@gmail.com', 'Engineer');
+test('create engineer object', () => {
+    const engineer = new Engineer('Dave', 0, 'asdf@gmail.com', 'davesGitHub');
 
     expect(engineer.name).toBe('Dave');
     expect(engineer.id).toEqual(expect.any(Number));
     expect(engineer.email).toBe('asdf@gmail.com');
     expect(engineer.role).toBe('Engineer')
+    expect(engineer.gitHub).toBe('davesGitHub');
 });
 
 test('gets engineer name', () => {
-    const engineer = new Engineer('Dave', 0, 'asdf@gmail.com', 'Engineer');
+    const engineer = new Engineer('Dave', 0, 'asdf@gmail.com', 'davesGitHub');
 
     expect(engineer.getName()).toEqual('Dave');
 });
 
 test('gets engineer ID number', () => {
-    const engineer = new Engineer('Dave', 0, 'asdf@gmail.com', 'Engineer');
+    const engineer = new Engineer('Dave', 0, 'asdf@gmail.com', 'davesGitHub');
 
     expect(engineer.getId()).toEqual(expect.any(Number));
 });
 
 test('gets engineer email', () => {
-    const engineer = new Engineer('Dave', 0, 'asdf@gmail.com', 'Engineer');
+    const engineer = new Engineer('Dave', 0, 'asdf@gmail.com', 'davesGitHub');
 
     expect(engineer.getEmail()).toBe('asdf@gmail.com');
 });
 
 test("gets engineer's role", () => {
-    const engineer = new Engineer('Dave', 0, 'asdf@gmail.com', 'Engineer');
+    const engineer = new Engineer('Dave', 0, 'asdf@gmail.com', 'davesGitHub');
 
     expect(engineer.getRole()).toBe('Engineer');
+});
+
+test("gets engineer's github", () => {
+    const engineer = new Engineer('Dave', 0, 'asdf@gmail.com', 'davesGitHub');
+
+    expect(engineer.getGithub()).toBe('davesGitHub');
 });
